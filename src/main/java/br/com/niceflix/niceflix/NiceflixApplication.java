ac33ec43a0800b75ac7b8b67ee4b096af8240ae9
arthurzvz <arthurzinnns7@gmail.com>
2025-12-19 20:56:12 -0300

    first commit

---
+++ src/main/java/br/com/niceflix/niceflix/NiceflixApplication.java
@@ -0,0 +1,13 @@
+package br.com.niceflix.niceflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiceflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/niceflix/niceflix/NiceflixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.niceflix.niceflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiceflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
