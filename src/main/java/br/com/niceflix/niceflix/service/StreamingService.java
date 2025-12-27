9cfbd1344e193745532f43c4404ef6968be6d7c3
arthurzvz <arthurzinnns7@gmail.com>
2025-12-26 23:06:25 -0300

    feat: adiciona StreamingService

--- src/main/java/br/com/niceflix/niceflix/service/StreamingService.java
+++ src/main/java/br/com/niceflix/niceflix/service/StreamingService.java
@@ -1,4 +1,31 @@
 package br.com.niceflix.niceflix.service;
 
+import br.com.niceflix.niceflix.entity.Streaming;
+import br.com.niceflix.niceflix.repository.StreamingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
 public class StreamingService {
+    private final StreamingRepository streamingRepository;
+
+    public List<Streaming> findAll(){
+        return streamingRepository.findAll();
+    }
+
+    public Streaming createStreaming(@RequestBody Streaming streaming){
+        return streamingRepository.save(streaming);
+    }
+    public Optional<Streaming> getByIdStreaming(Long id){
+        return streamingRepository.findById(id);
+    }
+    public void deleteByIdStreaming(Long id){
+        streamingRepository.deleteById(id);
+    }
+
 }
