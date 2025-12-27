711884669d7d2e0f8e6ac42e9681c63bed003c7a
arthurzvz <arthurzinnns7@gmail.com>
2025-12-26 23:07:25 -0300

    feat: entidade Streaming

--- src/main/java/br/com/niceflix/niceflix/entity/Streaming.java
+++ src/main/java/br/com/niceflix/niceflix/entity/Streaming.java
@@ -1,4 +1,19 @@
 package br.com.niceflix.niceflix.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "streaming")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Streaming {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
 }
